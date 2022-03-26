4a07c37d3185a71e3674c5931b8c495b888420dc
kh <rlaruagh@gmail.com>
2022-03-26 20:58:22 +0900

    tiktok server commit

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        url :"https://v16-webapp.tiktok.com/6f2ecd142b010018afb5ddae7a1478ce/623f23f3/video/tos/alisg/tos-alisg-pve-0037/eaf882009c0144dc87016eb9cfbbd3f1/?a=1988&br=5672&bt=2836&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-3m_nz7ThEsTJDXq&l=2022032608321101024500206906ACD961&lr=tiktok&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anJvbWg6ZmR3OzMzODgzNEApNDU8Zjg5Nzs1N2Q0ZTNmN2dqMnFycjRvXmRgLS1kLy1zczQzNDBeL142Xl41NS8vNGA6Yw%3D%3D&vl=&vr=",
+        channel : "@Jaymyung Lee Dance",
+        description : "#Elections",
+        song :"Bam Bam Babaabam",
+        likes : "123",
+        messages : "234",
+        shares: "465",
+    },
+    {
+        url :"https://v16-webapp.tiktok.com/4a973ae36c06105efa4e87a3c5c42a59/623f3b72/video/tos/alisg/tos-alisg-pve-0037/95387ad6233046caba67fc6d7cf67a5e/?a=1988&br=2872&bt=1436&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-3m_nz7ThH87JDXq&l=2022032610122801024408907110B8DB44&lr=tiktok&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anN1a2Q6ZnVvOzMzODgzNEApZzQ8ODM5ZTw5NzxmNmhmaWctcWptcjQwZzRgLS1kLy1zc2AxYDAwMjMxL2BhX18tYC06Yw%3D%3D&vl=&vr=",
+        channel :"@Bungubbang",
+        description :"#no pot",
+        song :"nuga mucna",
+        likes: "1123",
+        messages: "2324",
+        shares : "4654",
+    }
+]
+
+export default data;
---
+++ server.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import Data from './data.js';
+import Videos from './dbModel.js';
+
+const app = express();
+const port = 9000;
+
+
+const connection_url = 'mongodb+srv://kh:1234@cluster0.viqbh.mongodb.net/tiktok?retryWrites=true&w=majority';
+
+
+mongoose.connect(connection_url, {
+    tls : true
+});
+
+app.use(express.json())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'),
+    res.setHeader('Access-Control-Allow-Headers', '*'),
+    next()
+})
+
+app.post("/v2/posts" , (req, res) => {
+
+    const dbVideos = req.body
+
+    Videos.create(dbVideos, (err, data) => {
+        if(err){
+            res.status(500).send(err)
+        }else {
+            res.status(201).send(data)
+        }
+    })
+});
+
+app.get("/v2/posts" , (req, res) => {
+    Videos.find((err, data) => {
+
+        if(err){
+            res.status(500).send(err)
+        }else {
+            res.status(200).send(data)
+        }
+    })
+})
+
+
+app.get("/", (req, res) =>  res.status(200).send("hello world"));
+
+app.get("/v1/post", (req, res) => res.status(200).send(Data));
+
+app.listen(port, () => console.log("listening on localhost"));
+
+
